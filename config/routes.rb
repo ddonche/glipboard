53c47660be01b88ae401a9e7a7213eedd3b1d7f1
ddonche <dan@fatalfitness.com>
2017-05-03 03:07:22 +0000

    added and set up Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :blogs
   resources :glips
+  
+  root to: "glips#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
