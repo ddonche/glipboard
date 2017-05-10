d8db7a6763b9932b03eb90399710b626c3f50c32
ddonche <dan@fatalfitness.com>
2017-05-10 02:43:36 +0000

    fixed footer bug and link justification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
-  
-  before_filter :set_title
-  
-  def set_title
-    @page_title = "Glipboard | Go Glip Yourself"
-  end
+  include DefaultPageContent
 end
