c1ea1fcf67fc5d669202ef2fbaf473f36bd63f29
ddonche <dan@fatalfitness.com>
2017-05-17 02:33:16 +0000

    Styling for glip show page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
   
+  def resource_name
+    :user
+  end
+  
   wrap_parameters :user, include: [:username, :email, :password, :password_confirmation, :picture]
 
   def after_sign_up_path_for(resource)
