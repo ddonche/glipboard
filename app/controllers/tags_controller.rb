3e4e80d3618f9a8daea01cd52be1e25341f7fc60
ddonche <dan@fatalfitness.com>
2017-05-18 12:14:17 +0000

    fixed tag show page pagination issue

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,7 @@ class TagsController < ApplicationController
     @blogs = Blog.tagged_with(params[:tag])
     @glips = Glip.tagged_with(params[:tag])
     @everything = (@blogs + @glips).sort{|b,a| a.created_at <=> b.created_at }
-    @pager = Kaminari.paginate_array(@everything).page(params[:page]).per(2)
+    @pager = Kaminari.paginate_array(@everything).page(params[:page]).per(25)
   end
   
 end
