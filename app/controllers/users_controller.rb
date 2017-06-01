f2a223f6f8eb2fd1fc129bfa354244478b8e4aad
ddonche <dan@fatalfitness.com>
2017-06-01 04:39:06 +0000

    styling of index pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @page_title = "Glipboarders"
-    @users = User.all
+    @users = User.order('created_at DESC').page(params[:page]).per(20)
   end
   
   def show
