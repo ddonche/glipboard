896b1749b5d612728646a3d5075c9a6421dfb046
ddonche <dan@fatalfitness.com>
2017-06-08 16:04:58 +0000

    added follow form to user index

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,13 +5,13 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:followed_id])
     current_user.follow(user)
     voltaire_up(1, :reputation, user)
-    redirect_to user
+    redirect_back(fallback_location: user)
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
     voltaire_down(1, :reputation, user)
-    redirect_to user
+    redirect_back(fallback_location: user)
   end
 end
