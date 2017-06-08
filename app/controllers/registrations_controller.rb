86d1041d625a429d30cfdf1c25efdb535e5df956
ddonche <dan@fatalfitness.com>
2017-06-08 04:25:25 +0000

    redirect user after update, fixed some index issues

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,4 +15,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     users_path
   end
+  
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
 end
