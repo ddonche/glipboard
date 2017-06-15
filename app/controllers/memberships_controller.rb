1b4bc9f853220af514e3c809e62d6ee5dd6aab9e
ddonche <dan@fatalfitness.com>
2017-06-15 18:52:33 +0000

    only members can post and reply in groups

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,17 +1,19 @@
 class MembershipsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_group
 
   def create
     @user = current_user
-    @group = Group.friendly.find(params[:group_id])
     Membership.create!(user_id: @user.id, group_id: @group.id)
     redirect_to @group
   end
 
   def destroy
-    @group = Group.friendly.find(params[:group_id])
-    @membership = current_user.memberships.find_by(group_id: @group.id)
-    @membership.delete
+    current_user.memberships.find_by(group_id: @group.id).destroy
     redirect_to groups_path
+  end
+  
+  private
+  def set_group
+    @group = Group.friendly.find(params[:group_id])
   end
 end
