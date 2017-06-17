80679d039560a8068c7b742767b8f8d8a5aed1ce
ddonche <dan@fatalfitness.com>
2017-06-17 05:09:47 +0000

    cleaning up migrations for prod

--- db/migrate/20170522153631_rename_articles_to_blogs.rb
+++
@@ -1,9 +0,0 @@
-class RenameArticlesToBlogs < ActiveRecord::Migration[5.0]
-  def self.up
-    rename_table :articles, :blogs
-  end
-
-  def self.down
-    rename_table :blogs, :articles
-  end
-end
