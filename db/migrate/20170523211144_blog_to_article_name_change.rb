f2c7c429761d6a13b48b3325cc6a336133c919ed
ddonche <dan@fatalfitness.com>
2017-06-17 05:13:32 +0000

    cleaning up migrations for prod

--- db/migrate/20170523211144_blog_to_article_name_change.rb
+++ db/migrate/20170523211144_blog_to_article_name_change.rb
@@ -1,6 +1,5 @@
 class BlogToArticleNameChange < ActiveRecord::Migration[5.0]
   def change
     rename_table :blogs, :articles
-    rename_index :articles, :index_blogs_on_slug, :index_articles_on_slug
   end
 end
