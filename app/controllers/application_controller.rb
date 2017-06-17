26f695bb97e0e8d5b85ca1d561911d4451405606
ddonche <dan@fatalfitness.com>
2017-06-17 02:10:19 +0000

    styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include DefaultPageContent
+  
+  module Driftwood
+    # FOR PULLING RANDOM RECORD FROM DB
+    def driftwood(table, column)
+      @model = table.capitalize
+      @sum = User.count
+      @number = rand(1...@sum)
+      @final = User.limit(1).offset(@number)
+    end
+  end
 end
