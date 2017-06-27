d6d2311a8075dacabfdda8738859e92fbbe91f23
ddonche <dan@fatalfitness.com>
2017-06-27 22:44:46 +0000

    login redirect to old location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,22 @@ class ApplicationController < ActionController::Base
   def old_enough(user)
     (user.birthdate.to_date + 16.years) < Date.today
   end
+  # saves the location before loading each page so we can return to the
+  # right page. If we're on a devise page, we don't want to store that as the
+  # place to return to (for example, we don't want to return to the sign in page
+  # after signing in), which is what the :unless prevents
+  before_action :store_location, :unless => :devise_controller?
+
+  private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_location
+    # store last url as long as it isn't a /users path
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
+  end
+  
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
 end
