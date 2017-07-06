3c366c810e0f9fd3ed3fefd5fc11883228c8f30f
ddonche <dan@fatalfitness.com>
2017-07-06 01:00:21 +0000

    follow/unfollow notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   enum notification_type: { comment: 0, notation: 1, message: 2, feature: 3, marked_helpful: 4,
-                            participation: 5, response: 6, remark: 7 }
+                            participation: 5, response: 6, remark: 7, follow: 8, unfollow: 9 }
   enum read: { read: true, unread: false }
   belongs_to :notified_by, class_name: 'User'
 end
