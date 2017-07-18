61bc2e371ffd2e679f77753ba5c220a651225bdd
ddonche <dan@fatalfitness.com>
2017-07-18 08:52:17 +0000

    added podcasts to tag show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,9 +8,12 @@ class TagsController < ApplicationController
     @articles = Article.tagged_with(params[:tag])
     @glips = Glip.tagged_with(params[:tag])
     @groups = Group.tagged_with(params[:tag])
-    @everything = (@articles + @glips)
+    @podcasts = Podcast.tagged_with(params[:tag])
+    @everything = (@articles + @glips + @podcasts)
     @everything_plus = (@everything + @groups).sort{|b,a| a.created_at <=> b.created_at }
     @pager = Kaminari.paginate_array(@everything_plus).page(params[:page]).per(25)
   end
+  
+  private
   
 end
