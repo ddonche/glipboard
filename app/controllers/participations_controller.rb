e91b80bee24396039328df01ca5bdb903b668cdc
ddonche <dan@fatalfitness.com>
2018-05-29 02:49:42 +0000

    fixing email

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -7,7 +7,6 @@ class ParticipationsController < ApplicationController
     Participation.create!(user_id: @user.id, glip_id: @glip.id)
     Notification.create!(glip_id: @glip.id, recipient_id: @glip.user_id, 
                       notified_by_id: current_user.id, notification_type: "participation")
-        UserNotifier.send_joinglip_email(@glip.user).deliver
     voltaire_up(2, :reputation, @glip.user_id)
     Glip.create!(parent_id: @glip.id, title: @glip.title, content: @glip.content, 
                   completion_criteria: @glip.completion_criteria, user_id: @user.id)
@@ -16,6 +15,7 @@ class ParticipationsController < ApplicationController
       format.html { redirect_to glip_path(@last.id), notice: "You joined! Don't forget to customize it with your own content, milestones, tags, etc." }
       format.js { redirect_to glip_path(@last.id), notice: "You joined! Don't forget to customize it with your own content, milestones, tags, etc." }
     end
+    UserNotifier.send_joinglip_email(@glip.user).deliver
   end
 
   def destroy
