c0c4ad679a40a12c7511ac686544502bcdc593df
ddonche <dan@fatalfitness.com>
2019-07-11 06:41:56 +0000

    follower email

--- app/mailers/follow_mailer.rb
+++ app/mailers/follow_mailer.rb
@@ -5,9 +5,9 @@ class FollowMailer < ApplicationMailer
   #
   #   en.comment_mailer.new_comment.subject
   #
-  def new_follower_notification(user, follower)
+  def new_follower_notification(user, current_user)
     @user = user
-    @follower = follower
+    @follower = current_user
 
     mail to: @user.email, 
     subject: "#{@follower.username} is now following you on Glipboard!"
